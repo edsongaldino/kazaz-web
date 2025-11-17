dd1e1c96b4f3a4d7d7b35c8d8d2a7d56e9bd2f34
Edson Galdino <edsongaldino@outlook.com>
2025-11-16 20:04:30 -0400

    teste deploy automático front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,  // Define que é um ambiente de produção
-  apiUrl: 'https://api.seusite.com',  // URL da API para produção
+  apiUrl: 'https://api.kazaz.imb.br',  // URL da API para produção
 };
